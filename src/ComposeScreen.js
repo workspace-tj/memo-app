7efa40c4c542b32c8620dc37389fecd167c4ca9e
workspace-tj <workspace.tj@gmail.com>
2023-08-12 19:50:56 +0900

    test:余分な記述を削除。コードを整理。

--- src/ComposeScreen.js
+++ src/ComposeScreen.js
@@ -1,46 +1,45 @@
-import { Button, View, TextInput, StyleSheet, Text} from 'react-native';
+import { Button, View, TextInput, StyleSheet, Text } from 'react-native';
 import { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { storeItem } from './store';
 
 
 const ComposeScreen = ({ navigation }) => {
-    const [title, onChangeTitle] = useState();
-    const [content, onChangeContent] = useState();
-    const onPressStoreItem = (title, content, createdAt) => {
-      storeItem(title, content, createdAt);
-      navigation.navigate('Home');
-    }
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>タイトルを入力してください</Text>
-        <TextInput
-            style={styles.input}
-            onChangeText = {onChangeTitle}
-            value = {title}
-        />
-        <Text>メモを入力してください</Text>
-        <TextInput
-            style={styles.input}
-            onChangeText = {onChangeContent}
-            value = {content}
-        />
-        <Button
-          onPress={() => onPressStoreItem(title, content, Date.now())}
-          title="作成"
-        />
-      </View>
-    );
-  };
+  const [title, onChangeTitle] = useState();
+  const [content, onChangeContent] = useState();
+  const onPressStoreItem = (title, content, createdAt) => {
+    storeItem(title, content, createdAt);
+    navigation.navigate('Home');
+  }
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>タイトルを入力してください</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeTitle}
+        value={title}
+      />
+      <Text>メモを入力してください</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeContent}
+        value={content}
+      />
+      <Button
+        onPress={() => onPressStoreItem(title, content, Date.now())}
+        title="作成"
+      />
+    </View>
+  );
+};
 
-  const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      width: '90%',
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-  });
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: '90%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
-  export default ComposeScreen
+export default ComposeScreen
--- src/EditScreen.js
+++ src/EditScreen.js
@@ -1,58 +1,54 @@
-import { Button, View, TextInput, StyleSheet, Text} from 'react-native';
+import { Button, View, TextInput, StyleSheet, Text } from 'react-native';
 import { useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { mergeItem } from './store';
 
 
 const EditScreen = ({ route, navigation }) => {
-    const item = route.params;
-    const [title, onChangeTitle] = useState(item.title);
-    const [content, onChangeContent] = useState(item.content);
-    const onPressMergeItem = (title, content, createdAt, editedAt) => {
-      mergeItem(title, content, createdAt, editedAt);
-      navigation.navigate('Home');
+  const item = route.params;
+  const [title, onChangeTitle] = useState(item.title);
+  const [content, onChangeContent] = useState(item.content);
+  const onPressMergeItem = (title, content, createdAt, editedAt) => {
+    mergeItem(title, content, createdAt, editedAt);
+    navigation.navigate('Home');
+  }
+  useEffect(() => {
+    const set = () => {
+      onChangeTitle(item.title)
+      onChangeContent(item.content)
     }
-    useEffect(() => {
-      const set = () => {
-          onChangeTitle(item.title)
-          onChangeContent(item.content)
-        }
-      console.log({title,content})
-      
-      const unsubscribe = navigation.addListener('focus',set)
-  
-      return unsubscribe;
-    }, [navigation]);
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>タイトルを入力してください</Text>
-        <TextInput
-            style={styles.input}
-            onChangeText = {onChangeTitle}
-            value = {title}
-        />
-        <Text>メモを入力してください</Text>
-        <TextInput
-            style={styles.input}
-            onChangeText = {onChangeContent}
-            value = {content}
-        />
-        <Button
-          onPress={() => onPressMergeItem(title, content, item.createdAt, Date.now())}
-          title="更新"
-        />
-      </View>
-    );
-  };
+    const unsubscribe = navigation.addListener('focus', set)
+    return unsubscribe;
+  }, [navigation]);
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>タイトルを入力してください</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeTitle}
+        value={title}
+      />
+      <Text>メモを入力してください</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeContent}
+        value={content}
+      />
+      <Button
+        onPress={() => onPressMergeItem(title, content, item.createdAt, Date.now())}
+        title="更新"
+      />
+    </View>
+  );
+};
 
-  const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      width: '90%',
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-  });
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: '90%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
-  export default EditScreen
+export default EditScreen
